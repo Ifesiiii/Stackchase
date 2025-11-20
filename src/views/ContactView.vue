bd9410f926272424b73a73890fdc4f440d08b5a2
Ifesiiii <oluwanifesimi.makanjuola@stackchase.com>
2025-11-20 18:21:00 +0100

    Up to date

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,11 @@
 <script setup>
 import Hero from '../components/Contact/Hero.vue';
+import Form from '../components/Contact/Form.vue';
+import Footer from '../components/Footer.vue';
 </script>
 
 <template>
     <Hero />
+    <Form />
+    <Footer />
 </template>
---
+++ src/views/KitView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Hero from '../components/Company/Kit/Hero.vue';
+import Packs from '../components/Company/Kit/Packs.vue';
+import Inquiry from '../components/Company/Kit/Inquiry.vue';
+import Footer from '../components/Footer.vue'
+</script>
+
+<template>
+    <Hero />
+    <Packs />
+    <Inquiry />
+    <Footer />
+</template>
