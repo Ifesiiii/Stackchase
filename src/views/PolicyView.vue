9a524d8b13c7a0af293fd87fd5b5d6c55d79253e
Ifesiiii <oluwanifesimi.makanjuola@stackchase.com>
2025-12-06 21:28:45 +0100

    Updated changes

---
+++ src/views/PolicyView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Hero from '../components/Company/Policy/Hero.vue';
+import Content from '../components/Company/Policy/Content.vue';
+import Footer from '../components/Footer.vue';
+</script>
+
+
+<template>
+    <Hero />
+    <Content />
+    <Footer />
+</template>
