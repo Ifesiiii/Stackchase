8673679c64fec350f26db70c47722dd8c0757ed8
Ifesiiii <oluwanifesimi.makanjuola@stackchase.com>
2025-12-14 20:41:55 +0100

    Updated changes

--- src/views/PosiView.vue
+++ src/views/PosiView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Hero from '../components/Company/Career/Positions/Hero.vue';
 import Detail from '../components/Company/Career/Positions/Detail.vue';
+import Footer from '../components/Footer.vue';
 </script>
 
 <template>  
     <Hero />
     <Detail />
+    <Footer />
 </template>
