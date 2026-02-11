51bea73170c35d0144b38131cf606cdab52a2bb3
Ifesiiii <oluwanifesimi.makanjuola@stackchase.com>
2026-02-11 20:18:59 +0100

    Updated changes

--- src/components/Company/Career/Hero.vue
+++ src/components/Company/Career/Hero.vue
@@ -30,7 +30,7 @@ const scrollToPositions = () => {
         <Navtop />
         
         <!-- Hero Content -->
-        <div class="pb-32 pt-16 md:pt-24 lg:pt-[116px]">
+        <div class="mt-[24px] lg:mt-[80px] mb-[60px] lg:mb-[169px]">
           <div class="max-w-7xl">
             <p class="text-[#FFB343] text-lg sm:text-[24px] mb-6 sm:mb-8 leading-[24px] sm:leading-[28px] tracking-[0.2px]">Career</p>
             <h1 class="max-w-4xl text-3xl sm:text-[40px] md:text-[50px] lg:text-[60px] font-medium md:font-semibold lg:font-semibold text-white mb-8 sm:mb-12 lg:mb-[72px] leading-[40px] sm:leading-[50px] md:leading-[60px] lg:leading-[72px] tracking-[-1.5px] sm:tracking-[-2.56px]">
--- src/components/Company/Policy/Hero.vue
+++ src/components/Company/Policy/Hero.vue
@@ -4,7 +4,7 @@ import walls from '../../../assets/img/walls.png'
 </script>
 
 <template>
-    <section class="font-heading relative overflow-hidden mx-auto mb-[100px] max-w-screen-[1366px]">
+    <section class="font-heading relative overflow-hidden mx-auto  max-w-screen-[1366px]">
       <!-- Background Image Overlay -->
       <div class="absolute inset-0">
         <img 
@@ -20,7 +20,7 @@ import walls from '../../../assets/img/walls.png'
         <Navtop />
         
         <!-- Hero Content -->
-        <div class="pb-32 pt-16 md:pt-24 lg:pt-[116px]">
+        <div class="mt-[24px] lg:mt-[80px] mb-[96px] lg:mb-[200px]">
           <div class="max-w-7xl">
             <p class="text-[#FFB343] text-lg sm:text-[24px] mb-6 sm:mb-8 leading-[24px] sm:leading-[28px] tracking-[0.2px]">Privacy Policy</p>
             <h1 class="max-w-4xl text-3xl sm:text-[40px] md:text-[50px] lg:text-[60px] font-medium md:font-semibold lg:font-semibold text-white mb-8 sm:mb-12 lg:mb-[72px] leading-[40px] sm:leading-[50px] md:leading-[60px] lg:leading-[72px] tracking-[-1.5px] sm:tracking-[-2.56px]">
